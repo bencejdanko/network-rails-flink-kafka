336bb5e12f4d6c9afe14e7e53aefa3e7aed406ab
Jiyoon-sjsu <jiyoon.lee@sjsu.edu>
2025-04-18 12:52:28 -0700

    Update _tor.py

--- producer_src/pyxb_bindings/_tor.py
+++ producer_src/pyxb_bindings/_tor.py
@@ -24,7 +24,7 @@ _PyXBVersion = '1.2.6'
 _module_typeBindings = pyxb.utils.utility.Object()
 
 # Import bindings for namespaces imported into schema
-import _ct as _ImportedBinding__ct
+import pyxb_bindings._ct as _ImportedBinding__ct
 import pyxb.binding.datatypes
 
 # NOTE: All namespace declarations are reserved within the binding
